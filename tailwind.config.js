5d6219f2605252e6fc72369afc456b0af3736446
yliang123 <yliang24@ncsu.edu>
2022-07-02 15:14:17 -0400

    add dropzone

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}', './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tw-elements/dist/plugin')],
 };
