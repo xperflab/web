338390e91592b31c0dcc4c25503bcbb3b59ed1cd
yliang123 <yliang24@ncsu.edu>
2022-07-14 17:36:48 -0400

    update

--- src/App.js
+++ src/App.js
@@ -30,24 +30,3 @@ const App = observer(() => {
 });
 
 export default App;
-// /**
-//  *
-//  * @param { Component} value
-//  */
-// export default class App extends Component {
-//   const stores = {BarStore};
-//   // eslint-disable-next-line require-jsdoc
-//   render() {
-//     return (
-//       <Provider {...stores}>
-{/* <div className="h-full">
-  <LeftBar sideState = {this.state.sideState}/>
-  <ViewContainer sideState = {this.state.sideState}/>
-  <OpenFileDropezone/>
-</div>; */}
-//       </Provider>
-//     );
-//   }
-// }
-
-
