b145aa8d75fc0e0e69bea2220ddc6d04b6260498
yliang123 <yliang24@ncsu.edu>
2023-01-24 12:14:21 -0500

    update default color and 3d color

--- src/components/widget/DarkModeButton.js
+++ src/components/widget/DarkModeButton.js
@@ -4,49 +4,64 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
 /* This example requires Tailwind CSS v2.0+ */
-import {useEffect, useState} from 'react';
-import {inject, observer} from 'mobx-react';
-import {
-  MoonIcon, SunIcon,
-} from '@heroicons/react/outline';
+import { useEffect, useState } from "react";
+import { inject, observer } from "mobx-react";
+import { MoonIcon, SunIcon } from "@heroicons/react/outline";
 
 function DarkModeButton(props) {
   useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
-      props.ViewStore.setThemeDark();
-      // setTheme('dark');
-    } else {
-      props.ViewStore.setThemeLight();
-      // setTheme('light');
-    }
+    // if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+    //   props.ViewStore.setThemeDark();
+    //   // setTheme('dark');
+    // } else {
+    //   props.ViewStore.setThemeLight();
+    //   // setTheme('light');
+    // }
+    props.ViewStore.setThemeDark();
   }, []);
-  useEffect(() =>{
-    if (props.ViewStore.theme ==='dark') {
-      document.documentElement.classList.add('dark');
+  useEffect(() => {
+    if (props.ViewStore.theme === "dark") {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   });
-  const handleThemeSwitch =()=> {
-    if (props.ViewStore.theme === 'dark') {
+  const handleThemeSwitch = () => {
+    if (props.ViewStore.theme === "dark") {
       props.ViewStore.setThemeLight();
     } else {
       props.ViewStore.setThemeDark();
     }
     props.FlameGraphStore.incrementFlamegraphId();
-    localStorage.setItem('theme', props.ViewStore.theme);
+    localStorage.setItem("theme", props.ViewStore.theme);
     // setTheme(theme === 'dark'? 'light' : 'dark');
   };
   return (
     <>
-      <button type='button' onClick={handleThemeSwitch} className ="dark:bg-[#1E1D2B]  pr-2">
-        {props.ViewStore.theme === 'dark' ? <MoonIcon className="h-6 w-6
-                     text-[#B73793] bg-[#1E1D2B]  " aria-hidden="true" /> : <SunIcon className="h-6 w-6
-                     text-[#B73793]" aria-hidden="true" />
-        }
+      <button
+        type="button"
+        onClick={handleThemeSwitch}
+        className="dark:bg-[#1E1D2B]  pr-2"
+      >
+        {props.ViewStore.theme === "dark" ? (
+          <MoonIcon
+            className="h-6 w-6
+                     text-[#B73793] bg-[#1E1D2B]  "
+            aria-hidden="true"
+          />
+        ) : (
+          <SunIcon
+            className="h-6 w-6
+                     text-[#B73793]"
+            aria-hidden="true"
+          />
+        )}
       </button>
     </>
   );
 }
-export default inject('ViewStore', 'ProfileStore', 'FlameGraphStore',
+export default inject(
+  "ViewStore",
+  "ProfileStore",
+  "FlameGraphStore"
 )(observer(DarkModeButton));
